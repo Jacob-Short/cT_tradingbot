b53a6e911c55cef0b78e3253eb2bc055a2a83f4b
Jacob Short <thatmanjshort@gmail.com>
2021-11-18 15:52:17 -0800

    updated data loop, need to add card on binance

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import sqlalchemy
 
 import mysecrets
 
+import binance
 from binance import AsyncClient, BinanceSocketManager
 from binance.client import Client
 
 import sys
+import time
 import os
 import logging
 import signal
@@ -32,6 +34,7 @@ def create_parser():
         "-f", "--feed", help="feed database with data", action="store_true"
     )
     parser.add_argument("symbol", help="symbol of crypto to look up")
+    parser.add_argument("pulls", help="how many entries to pull")
 
     return parser
 
@@ -42,34 +45,36 @@ async def main(args):
     ns = parser.parse_args(args)
 
     feed = ns.feed
-    symbol = ns.symbol
+    symbol = ns.symbol.upper()
+    pulls = int(ns.pulls)
 
     if not ns:
         parser.print_usage()
 
     if feed:
-        await feed_data(symbol)
+        await feed_data(symbol, pulls)
 
 
-async def feed_data(sym):
+async def feed_data(sym, pulls):
     bsm = BinanceSocketManager(client)
     bin_sym = f"{sym}USDT"
     socket = bsm.trade_socket(bin_sym)
     async with socket as bsm_socket:
         print("Generating data...")
-        for _ in range(60):
-            response = await bsm_socket.recv()
-            # print(response)
-            df = create_data_frame(response)
-            df.to_sql(f"cT_{sym}", db_engine, if_exists="append", index=False)
-            sql_df = pd.read_sql(f"cT_{sym}", db_engine)
-            print(sql_df)
-            print("downloading...")
+        while True:
+            for _ in range(pulls):
+                response = await bsm_socket.recv()
+                # print(response)
+                df = create_data_frame(response)
+                df.to_sql(f"cT_{sym}", db_engine, if_exists="append", index=False)
+                sql_df = pd.read_sql(f"cT_{sym}", db_engine)
+                print(sql_df)
+                print("downloading...")
 
-
-            client.close_connection()
-        # os.system('clear')
-        strategy(0.001, 60, 0.001, sym)
+                client.close_connection()
+            # os.system('clear')
+            time.sleep(1)
+            strategy(0.0001, pulls, 0.001, sym)
 
 
 def create_data_frame(rs):
@@ -93,30 +98,35 @@ def strategy(entry, lookback, qty, sym, open_pos=False):
     """
     bin_sym = f"{sym}USDT"
     # while True:
-    print('Developing strategy...')
+
+    print("Developing strategy...")
+    time.sleep(1)
 
     sql_df = pd.read_sql(f"cT_{sym}", db_engine)
     # looking back at the last 60 valid entries in db
     lookbackperiod = sql_df.iloc[-lookback:]
     # print(lookbackperiod.Price.pct_change())
-    print('Accumilating returns...')
+    print("Accumilating returns...")
+    time.sleep(1)
     # accumilating the returns from the lookbackperiod
     cum_ret = (lookbackperiod.Price.pct_change() + 1).cumprod() - 1
-    print('Returns completed')
+    print("Returns completed")
+    time.sleep(1)
     print(f"Accumilated Returns:\n{cum_ret}")
     print("Done")
+    time.sleep(1)
+
     if not open_pos:
         if cum_ret[cum_ret.last_valid_index()] > entry:
-            # loop getting stuck on this line ^^^
-            # TODO:
-            # needs to interval between feed and strat otherwise will
-            # always look at last 60 entries
-            order = client.create_order(
-                symbol=bin_sym, side="BUY", TYPE="market", quantity=qty
-            )
-            print(order)
-            open_pos = True
-            # break
+            try:
+                order = client.create_order(
+                    symbol=bin_sym, side="BUY", TYPE="market", quantity=qty
+                )
+                print(order)
+                open_pos = True
+                # break
+            except binance.exceptions.BinanceAPIException:
+                print("You do not have permission to BUY")
 
     if open_pos:
         while True:
